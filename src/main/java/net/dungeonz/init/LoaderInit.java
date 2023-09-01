584cbdd73dc3559bb2723a7b269075819ae37058
Globox1997 <mitschke11@web.de>
2023-09-01 15:12:41 +0200

    changed name

--- src/main/java/net/dungeonz/init/LoaderInit.java
+++ src/main/java/net/dungeonz/init/LoaderInit.java
@@ -4,7 +4,7 @@ import net.dungeonz.data.DungeonLoader;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.resource.ResourceType;
 
-public class JsonReaderInit {
+public class LoaderInit {
 
     public static void init() {
         ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new DungeonLoader());
